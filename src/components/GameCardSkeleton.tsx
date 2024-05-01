da89ddbd760e4a04ce9e719a22de66ebf02b4c7f
Biswajeet Sahoo <2000biswajeet@gmail.com>
2024-05-01 12:35:22 +0530

    Show loading skeleton

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import React from "react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width={"280px"} borderRadius={"10px"} overflow={"hidden"}>
+      <Skeleton height={"200px"} />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
