c5b9f889e54861295a81d4b1acc4657cbee0f957
Biswajeet Sahoo <2000biswajeet@gmail.com>
2024-05-07 12:11:22 +0530

    Sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -55,7 +56,12 @@ function App() {
                 setGameQuery({ ...gameQuery, platform })
               }
             />
-            <SortSelector />
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectedSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+            />
           </HStack>
           <GameGrid gameQuery={gameQuery} />
         </GridItem>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,7 +24,8 @@ const useGames = (gameQuery: GameQuery) =>
   useData<Game>('/games', {
     params: {
       genres: gameQuery.genre?.id, 
-      platforms: gameQuery.platform?.id
+      platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder
     }}, 
     [gameQuery])
 
